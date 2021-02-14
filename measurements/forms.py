c5d84fcf85ea77caf448ef52cd056ed1e627eccf
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-02-14 17:17:43 +0530

    Add files via upload

---
+++ measurements/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Measurements
+
+
+class MeasurementModelForm(forms.ModelForm):
+    class Meta:
+        model = Measurements
+        fields = ('destination', )
+ 
